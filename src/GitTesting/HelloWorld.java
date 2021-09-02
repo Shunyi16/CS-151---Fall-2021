1ab711cf84684ce8a929937dcb348b22d01d8132
“Shunyi <Shunyi.liu16@gmail.com>
2021-09-02 15:52:59 -0700

    added date

--- src/GitTesting/HelloWorld.java
+++ src/GitTesting/HelloWorld.java
@@ -3,5 +3,6 @@ package GitTesting;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("date: 9/2/2021");
     }
 }
